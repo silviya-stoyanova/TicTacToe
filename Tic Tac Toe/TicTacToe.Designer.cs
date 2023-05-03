cda986a78c664a00fd381892a688d76e46d62337
Siviya Stoyanova <silviya.stoyanova@siili.com>
2023-05-03 17:03:44 +0300

    Check for winner. Update scores accordingly.

--- Tic Tac Toe/TicTacToe.Designer.cs
+++ Tic Tac Toe/TicTacToe.Designer.cs
@@ -70,7 +70,7 @@
             this.box1.Size = new System.Drawing.Size(110, 110);
             this.box1.TabIndex = 0;
             this.box1.UseVisualStyleBackColor = false;
-            this.box1.Click += new System.EventHandler(this.buttonClick);
+            this.box1.Click += new System.EventHandler(this.boxClick);
             // 
             // box2
             // 
@@ -89,7 +89,7 @@
             this.box2.Size = new System.Drawing.Size(110, 110);
             this.box2.TabIndex = 1;
             this.box2.UseVisualStyleBackColor = false;
-            this.box2.Click += new System.EventHandler(this.buttonClick);
+            this.box2.Click += new System.EventHandler(this.boxClick);
             // 
             // box4
             // 
@@ -108,7 +108,7 @@
             this.box4.Size = new System.Drawing.Size(110, 110);
             this.box4.TabIndex = 3;
             this.box4.UseVisualStyleBackColor = false;
-            this.box4.Click += new System.EventHandler(this.buttonClick);
+            this.box4.Click += new System.EventHandler(this.boxClick);
             // 
             // box5
             // 
@@ -127,7 +127,7 @@
             this.box5.Size = new System.Drawing.Size(110, 110);
             this.box5.TabIndex = 4;
             this.box5.UseVisualStyleBackColor = false;
-            this.box5.Click += new System.EventHandler(this.buttonClick);
+            this.box5.Click += new System.EventHandler(this.boxClick);
             // 
             // box6
             // 
@@ -146,7 +146,7 @@
             this.box6.Size = new System.Drawing.Size(110, 110);
             this.box6.TabIndex = 5;
             this.box6.UseVisualStyleBackColor = false;
-            this.box6.Click += new System.EventHandler(this.buttonClick);
+            this.box6.Click += new System.EventHandler(this.boxClick);
             // 
             // box7
             // 
@@ -165,7 +165,7 @@
             this.box7.Size = new System.Drawing.Size(110, 110);
             this.box7.TabIndex = 6;
             this.box7.UseVisualStyleBackColor = false;
-            this.box7.Click += new System.EventHandler(this.buttonClick);
+            this.box7.Click += new System.EventHandler(this.boxClick);
             // 
             // box8
             // 
@@ -184,7 +184,7 @@
             this.box8.Size = new System.Drawing.Size(110, 110);
             this.box8.TabIndex = 7;
             this.box8.UseVisualStyleBackColor = false;
-            this.box8.Click += new System.EventHandler(this.buttonClick);
+            this.box8.Click += new System.EventHandler(this.boxClick);
             // 
             // newGameBtn
             // 
@@ -293,7 +293,7 @@
             this.box9.Size = new System.Drawing.Size(110, 110);
             this.box9.TabIndex = 17;
             this.box9.UseVisualStyleBackColor = false;
-            this.box9.Click += new System.EventHandler(this.buttonClick);
+            this.box9.Click += new System.EventHandler(this.boxClick);
             // 
             // box3
             // 
@@ -312,7 +312,7 @@
             this.box3.Size = new System.Drawing.Size(110, 110);
             this.box3.TabIndex = 19;
             this.box3.UseVisualStyleBackColor = false;
-            this.box3.Click += new System.EventHandler(this.buttonClick);
+            this.box3.Click += new System.EventHandler(this.boxClick);
             // 
             // pictureBox2
             // 
--- Tic Tac Toe/TicTacToe.cs
+++ Tic Tac Toe/TicTacToe.cs
@@ -9,33 +9,53 @@ namespace Tic_Tac_Toe
 
         public string currentPlayer = "X";
         public int currentTurn = 1;
+        public int minTurnsRequiredToScore = 5;
         public int maxTurns = 9;
         public int xScoreCount = 0;
         public int oScoreCount = 0;
         public int drawsCount = 0;
 
-        private void buttonClick(object sender, EventArgs e)
+        private void TicTacToe_Load(object sender, EventArgs e)
+        {
+            xScore.Text = $"X score: {xScoreCount}";
+            oScore.Text = $"O score: {oScoreCount}";
+            draws.Text = $"Draws:   {drawsCount}";
+        }
+
+        private void boxClick(object sender, EventArgs e)
         {
             Button button = (Button)sender;
+            bool boxIsEmpty = string.IsNullOrWhiteSpace(button.Text);
 
-            if (string.IsNullOrWhiteSpace(button.Text))
+            if (boxIsEmpty)
             {
-
-
                 if (currentPlayer == "X")
                 {
                     button.Text = "X";
+
+                    bool hasWinner = CheckForWinner();
+                    if (currentTurn >= minTurnsRequiredToScore && hasWinner)
+                    {
+                        xScoreCount++;
+                        xScore.Text = $"X score: {xScoreCount}";
+                        restartGameBtn_Click(sender, e);
+                    }
+
                     currentPlayer = "O";
                 }
                 else
                 {
                     button.Text = "O";
+
+                    bool hasWinner = CheckForWinner();
+                    if (currentTurn >= minTurnsRequiredToScore && hasWinner)
+                    {
+                        oScoreCount++;
+                        oScore.Text = $"O score: {oScoreCount}";
+                        restartGameBtn_Click(sender, e);
+                    }
+
                     currentPlayer = "X";
-                }
-
-                if (currentTurn == maxTurns)
-                {
-
                 }
 
                 currentTurn++;
@@ -43,11 +63,48 @@ namespace Tic_Tac_Toe
 
         }
 
-        private void TicTacToe_Load(object sender, EventArgs e)
+        private bool CheckForWinner()
         {
-            xScore.Text = $"X score: {xScoreCount}";
-            oScore.Text = $"O score: {oScoreCount}";
-            draws.Text = $"Draws:   {drawsCount}";
+            // horizontal
+            if (box1.Text == box2.Text && box1.Text == box3.Text && !string.IsNullOrWhiteSpace(box1.Text))
+            {
+                return true;
+            }
+            else if (box4.Text == box5.Text && box4.Text == box6.Text && !string.IsNullOrWhiteSpace(box4.Text))
+            {
+                return true;
+            }
+            else if (box7.Text == box8.Text && box7.Text == box9.Text && !string.IsNullOrWhiteSpace(box7.Text))
+            {
+                return true;
+            }
+
+            // vertical
+            if (box1.Text == box4.Text && box1.Text == box7.Text && !string.IsNullOrWhiteSpace(box1.Text))
+            {
+                return true;
+            }
+            else if (box2.Text == box5.Text && box2.Text == box8.Text && !string.IsNullOrWhiteSpace(box2.Text))
+            {
+                return true;
+            }
+            else if (box3.Text == box6.Text && box3.Text == box9.Text && !string.IsNullOrWhiteSpace(box3.Text))
+            {
+                return true;
+            }
+
+            // diagonal
+            if (box1.Text == box5.Text && box1.Text == box9.Text && !string.IsNullOrWhiteSpace(box1.Text))
+            {
+                return true;
+            }
+            else if (box3.Text == box5.Text && box3.Text == box7.Text && !string.IsNullOrWhiteSpace(box3.Text))
+            {
+                return true;
+            }
+
+            return false;
+
         }
 
         private void restartGameBtn_Click(object sender, EventArgs e)
@@ -73,6 +130,11 @@ namespace Tic_Tac_Toe
             xScoreCount = 0;
             oScoreCount = 0;
             drawsCount = 0;
+
+            xScore.Text = $"X score: {xScoreCount}";
+            oScore.Text = $"O score: {oScoreCount}";
+            draws.Text = $"Draws:   {drawsCount}";
+
         }
 
         private void exitBtn_Click(object sender, EventArgs e)
