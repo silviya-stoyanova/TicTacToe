f15a3b7302c943efde870813f16206268ba89764
Siviya Stoyanova <silviya.stoyanova@siili.com>
2023-05-03 16:13:58 +0300

    Implement restart game, new game and close functionalities.

--- Tic Tac Toe/TicTacToe.cs
+++ Tic Tac Toe/TicTacToe.cs
@@ -12,43 +12,72 @@ namespace Tic_Tac_Toe
         public int maxTurns = 9;
         public int xScoreCount = 0;
         public int oScoreCount = 0;
+        public int drawsCount = 0;
 
         private void buttonClick(object sender, EventArgs e)
         {
             Button button = (Button)sender;
 
-            if (currentPlayer == "X")
+            if (string.IsNullOrWhiteSpace(button.Text))
             {
-                button.Text = "X";
-                currentPlayer = "O";
-            }
-            else
-            {
-                button.Text = "O";
-                currentPlayer = "X";
+
+
+                if (currentPlayer == "X")
+                {
+                    button.Text = "X";
+                    currentPlayer = "O";
+                }
+                else
+                {
+                    button.Text = "O";
+                    currentPlayer = "X";
+                }
+
+                if (currentTurn == maxTurns)
+                {
+
+                }
+
+                currentTurn++;
             }
 
-            currentTurn++;
         }
 
         private void TicTacToe_Load(object sender, EventArgs e)
         {
-
+            xScore.Text = $"X score: {xScoreCount}";
+            oScore.Text = $"O score: {oScoreCount}";
+            draws.Text = $"Draws:   {drawsCount}";
         }
 
         private void restartGameBtn_Click(object sender, EventArgs e)
         {
+            box1.Text = "";
+            box2.Text = "";
+            box3.Text = "";
+            box4.Text = "";
+            box5.Text = "";
+            box6.Text = "";
+            box7.Text = "";
+            box8.Text = "";
+            box9.Text = "";
 
+            currentPlayer = "X";
+            currentTurn = 1;
         }
 
         private void newGameBtn_Click(object sender, EventArgs e)
         {
+            restartGameBtn_Click(sender, e);
 
+            xScoreCount = 0;
+            oScoreCount = 0;
+            drawsCount = 0;
         }
 
         private void exitBtn_Click(object sender, EventArgs e)
         {
-
+            Close();
         }
     }
 }
